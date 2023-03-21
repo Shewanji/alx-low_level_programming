cd6d27cbf1ff24a37a800387ff182212354c1355
Shewanji <marrionwanjiru26@gmail.com>
2023-03-21 03:26:31 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main -main function is the starting point of a program
+ *
+ * _putchar: writes the character to stdout
+ *
+ * Return: always 0 when successful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
