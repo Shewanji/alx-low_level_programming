08c7bbfcb4701aeb8e8a343d6254b317ff5b7698
Shewanji <marrionwanjiru26@gmail.com>
2023-06-19 01:28:09 -0700

    create dynamic library

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: string to be counted
+ *
+ * Return: count, the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
